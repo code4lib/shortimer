08ec9ad01f0393fab559f916875e84955d9707f1
Ed Summers <ehs@pobox.com>
2012-01-26 04:35:16 +0000

    added updated field to Job to use when ordering sitemap

--- jobs/sitemap.py
+++ jobs/sitemap.py
@@ -2,12 +2,12 @@ from django.contrib.sitemaps import Sitemap
 from shortimer.jobs.models import Job
 
 class JobSitemap(Sitemap):
-    changefreq = "never"
+    changefreq = "always"
     priority = 0.9
 
     def items(self):
         jobs = Job.objects.filter(published__isnull=False)
-        return jobs.order_by('-published')
+        return jobs.order_by('-updated')
 
     def lastmod(self, obj):
-        return obj.published
+        return obj.updated
