8f502820399ab73f6f388d2b493d188cf9e85cb4
Ed Summers <ehs@pobox.com>
2012-01-26 04:13:34 +0000

    might as well order the jobs in the sitemap

--- jobs/sitemap.py
+++ jobs/sitemap.py
@@ -6,7 +6,8 @@ class JobSitemap(Sitemap):
     priority = 0.9
 
     def items(self):
-        return Job.objects.filter(published__isnull=False)
+        jobs = Job.objects.filter(published__isnull=False)
+        return jobs.order_by('-published')
 
     def lastmod(self, obj):
         return obj.published
