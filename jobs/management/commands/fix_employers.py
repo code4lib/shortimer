1721f528dfdd22f55a3b5cd0fa3eafa288399554
Ed Summers <ehs@pobox.com>
2013-02-24 17:51:24 +0000

    improved fixing, which will favor employers with addresses now

--- jobs/management/commands/fix_employers.py
+++ jobs/management/commands/fix_employers.py
@@ -14,23 +14,35 @@ class Command(BaseCommand):
             employers = list(Employer.objects.filter(slug=slug['slug']))
             with_freebase_id = []
             without_freebase_id = []
+            with_city = []
+            without_city = []
             
             for employer in employers:
+
                 if employer.freebase_id.startswith("/en/"):
                     with_freebase_id.append(employer)
                 else:
                     without_freebase_id.append(employer)
 
-            if len(with_freebase_id) != 1:
-                print "uncertain %s - %s" % (slug['slug'], with_freebase_id)
-                continue
+                if employer.city:
+                    with_city.append(employer)
+                else:
+                    without_city.append(employer)
 
-            e1 = with_freebase_id[0]
-            for e2 in without_freebase_id:
-                for job in e2.jobs.all():
-                    print job
-                    job.employer = e1
-                    job.save()
-                e2.delete()
+            if len(with_freebase_id) > 1 and len(with_city) == 1:
+                fix(with_city[0], without_city)
+
+            elif len(with_freebase_id) == 1:
+                fix(with_freebase_id[0], without_freebase_id)
+
+def fix(good_employer, bad_employers):
+    for bad_employer in bad_employers:
+        for job in bad_employer.jobs.all():
+            print  "rewiring %s to %s" % (job, good_employer)
+            job.employer = good_employer
+            job.save()
+        bad_employer.delete()
+        print "deleting: %s" % bad_employer
+
 
             
